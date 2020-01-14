aab6af53f3a0c302ac4f83bbfab9c48956cb8ff5
superhzf <superhzf@hotmail.com>
2020-01-14 11:15:03 -0800

    update cv

--- src/cross_validation.py
+++ src/cross_validation.py
@@ -5,9 +5,9 @@ import pandas as pd
 --- binary classification
 --- multi class classification
 - multi label classification
-- single column regression
-- multi column regression
-- holdout based
+--- single column regression
+--- multi column regression
+--- holdout based, this is important for time series data
 """
 
 
@@ -18,7 +18,7 @@ class CrossValidation:
             target_cols,
             problem_type='binary_classification',
             num_folds=5,
-            shuffle=True,
+            shuffle,
             random_state=42):
         self.dataframe = df
         self.target_cols = target_cols
@@ -47,13 +47,23 @@ class CrossValidation:
                 for fold,(train_idx,val_idx) in enumerate(kf.split(X=self.dataframe,y=self.dataframe[target].values)):
                     self.dataframe.loc[val_idx,'kfold'] = fold
 
-        elif self.problem_type == 'single_col_regression':
-            if self.num_target != 1:
+        elif self.problem_type in ('single_col_regression','multicol_regression'):
+            if self.num_target != 1 and self.problem_type == 'single_col_regression':
                 raise Exception('Invalid number of targets for {} problem!'.format(self.problem_type))
-            target = self.target_cols[0]
+            if self.num_target < 2 and self.problem_type == 'multicol_regression':
+                raise Exception('Invalid number of targets for {} problem!'.format(self.problem_type))
             kf = model_selection.KFold(n_splits = self.num_folds)
             for fold,(train_idx,val_idx) in enumerate(kf.split(X=self.dataframe,y=self.dataframe[target].values)):
                 self.dataframe.loc[val_idx,'kfold'] = fold
+
+        elif self.problem_type.startswith('holdout_'):
+            # holdout_5, holdout_10
+            holdout_percentage = int(self.problem_type.split('_')[1])
+            num_holdout_samples = int(len(self.dataframe)*holdout_percentage*0.01)
+            # in order for this to work, your data should be ordered by time factors
+            self.dataframe.loc[:len(self.dataframe)-num_holdout_samples,'kfold']=0
+            self.dataframe.loc[len(self.dataframe)-num_holdout_samples:,'kfold']=1 #holdout set,
+
 
         else:
             raise Exception('Problem type not understood')
@@ -63,7 +73,7 @@ class CrossValidation:
 
 if __name__ == '__main__':
     df = pd.read_csv('../input/train.csv')
-    cv = CrossValidation(df,target_cols = ['target'])
+    cv = CrossValidation(df,target_cols = ['target'],problem_type='holdout_10')
     df_split = cv.split()
     print(df_split.head())
     print(df_split.kfold.value_counts())
